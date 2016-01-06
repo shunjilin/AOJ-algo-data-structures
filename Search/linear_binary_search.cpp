5d944d8f8564ea82dfc8d123b04cf157a157a56e
Shunji Lin <lgxshunji@gmail.com>
2016-01-06 18:08:46 +0900

    add linear_binary_search and dictionary

---
+++ Search/linear_binary_search.cpp
@@ -0,0 +1,41 @@
+/**
+   <Linear Search / Binary Search>
+   
+   http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_4_A
+
+   http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_4_B
+   
+   Shunji Lin
+   06/01/2016
+**/
+
+#include <cstdio>
+#include <set>
+
+int main()
+{
+  int s, t; //number of integers for S and T
+
+  int count = 0; // number of integers of T in S
+  
+  std::set<int> S; // set S to be checked against
+
+  scanf("%d", &s);
+  
+  for (int i = 0; i < s; ++i) {
+    int input;
+    scanf("%d", &input);
+    S.insert(input);
+  }
+
+  scanf("%d", &t);
+
+  for (int i = 0; i < t; ++i) {
+    int input;
+    scanf("%d", &input);
+    if ( S.find(input) != S.end() ) ++count; // increment count if found in S
+  }
+
+  printf("%d\n", count);
+}
+    
